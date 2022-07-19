09940e5f087ce4d27043565377a069f1aa8639b8
valgars <alexandr.pylypchuk@gmail.com>
2022-07-19 15:08:28 +0300

    update

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -8,15 +8,19 @@ const CharacterCard = ({ result }) => {
         (result.status === 'Alive' ? statusColor = 'success' : statusColor = 'secondary')
     return (
         <div className="card mt-3 col-5 p-0 border-dark text-center" >
-            <div className="row g-7">
-                <div className="col-md-4 ">
+            <div className="row g-0">
+                <div className="col-md-6 ">
                     <img src={result.image} className="img-fluid rounded-start " alt='character' />
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-6">
                     <div className="card-body">
                         <h5 className="card-title">{result.name}</h5>
-                        <span className={`badge text-bg-${statusColor}`}>{result.status}</span>
-                        <p className="card-text mb-1"><small className="text-muted">Last known location:</small></p>
+                        <div className='d-flex justify-content-center'>
+                            <span className={`badge text-bg-${statusColor} p-2 `}>
+                                {result.status}
+                            </span> - <p className='card-text'>{result.species}</p>
+                        </div>
+                        <p className="card-text my-3"><small className="text-muted">Last known location:</small></p>
                         <p className="card-text">{result.location.name}</p>
                         <p className="card-text mb-1"><small className="text-muted">First seen in:</small></p>
                         <p className="card-text">{result.location.name}</p>

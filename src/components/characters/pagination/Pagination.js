834d1e1734b48c130114c26148f9b62c89d9396d
valgars <alexandr.pylypchuk@gmail.com>
2022-07-19 22:57:09 +0300

    asyncAwait

--- src/components/characters/pagination/Pagination.js
+++ src/components/characters/pagination/Pagination.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactPaginate from 'react-paginate';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
-  return (
+   return (
     <ReactPaginate
-      breakLabel="..."
-      nextLabel=">>"
-      pageCount={42}
-      previousLabel="<<"
-      renderOnZeroPageCount={null}
-
-    />
+        
+      />
   )
 }
 

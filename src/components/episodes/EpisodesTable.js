6f351c81e2292abf2db6d077e18d891e8f8ead6f
valgars <alexandr.pylypchuk@gmail.com>
2022-08-17 21:55:30 +0300

    updt

---
+++ src/components/episodes/EpisodesTable.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const EpisodesTable = ({ results, page }) => {
+    return (
+        <table className='table table-bordered'>
+
+            <thead className='table-dark'>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Episode</th>
+                    <th scope="col">Air Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {results?.map(result => {
+                    return (
+                        <tr key={result.id}>
+                            <td>{result.id}</td>
+                            <td>{result.name}</td>
+                            <td>{result.episode}</td>
+                            <td>{result.air_date}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+
+        </table >
+    )
+}
+
+export default EpisodesTable;
--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import Spinner from '../components/ui/Spinner';
+import React, { useState, useEffect } from 'react';
+import  EpisodesTable  from '../components/episodes/EpisodesTable';
+import ReactPaginate from 'react-paginate';
 
-const Episodes = () => {
+const Episodes  = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [episodes, setEpisodes] = useState([]);
+  const { info, results } = episodes;
+
+  let urlEpisodes = `https://rickandmortyapi.com/api/episode?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(urlEpisodes).then((res) => res.json());
+      setEpisodes(data);
+    })();
+  }, [urlEpisodes]);
+
+  const onPageChange = (item) => {
+    setPageNumber(item.selected + 1);
+  }
+ 
   return (
-      <div className='container my-5'>
-          <Spinner/>
-          <h2 className='text-center my-2'>Here will be Episodes page...</h2>
+    <div className='container'>
+      <div className='my-5 col-10 text-center table-responsive w-100'>
+        <EpisodesTable
+          results={results}
+          page='/episodes/'
+        />
+  
+        <ReactPaginate
+          pageCount={info?.pages || 3}
+          onPageChange={onPageChange}
+          forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+          className="pagination justify-content-center my-5 gap-2 "
+          previousClassName="btn fs-5 "
+          nextClassName="btn fs-5 "
+          activeClassName="active"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+        />
       </div>
+    </div>
   )
 }
 
-export default Episodes
+
+export default Episodes;
